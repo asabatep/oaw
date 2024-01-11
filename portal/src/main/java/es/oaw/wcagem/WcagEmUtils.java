291ed9bac812ec2c8e4044c0f8a43f4be81552d3
Pablo <pablo.gonzalez@fundacionctic.org>
2024-01-11 14:04:42 +0100

    Añadir nuevas verificaciones + corregir bugs generacion IRAS

--- portal/src/main/java/es/oaw/wcagem/WcagEmUtils.java
+++ portal/src/main/java/es/oaw/wcagem/WcagEmUtils.java
@@ -608,7 +608,6 @@ public final class WcagEmUtils {
 		List<ProblemForm> problemsrealtedThisWcagPoint = new ArrayList<>();
 		if (problems != null) {
 			for (ProblemForm problem : problems) {
-				
 				if (checkWcagRelationMap.get(wcagEmId) != null && ((List<String>) checkWcagRelationMap.get(wcagEmId)).contains(problem.getCheck())) {
 					problemsrealtedThisWcagPoint.add(problem);
 				}
@@ -736,9 +735,9 @@ public final class WcagEmUtils {
 		}
 		boolean allEqual = new HashSet<Integer>(integerList).size() <= 1;
 		ValidationDetails validationDetailes = new ValidationDetails();
-		if (allEqual) {
+		/*if (allEqual) {
 			processSimpleVerification(tmpWcag, observatorySubgroupForms.get(0), wcagEmId, checkChecks);
-		} else if (integerList.contains(Constants.OBS_VALUE_RED_ZERO)) {
+		} else*/ if (integerList.contains(Constants.OBS_VALUE_RED_ZERO)) {
 			Map<String, List<String>> checkWcagRelationMap = checkWcagRelationMap();
 			validationDetailes.setResult(EARL_CANNOT_TELL);
 			// Detail errors
