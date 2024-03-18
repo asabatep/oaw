feba4f7e2c7b376f927b8f772e42bef0f578fa6a
Pablo <pablo.gonzalez@fundacionctic.org>
2024-03-18 09:15:41 +0100

    Fix missing permission

--- portal/scripts/5.8.0/ALTER_OAW_5.8.0.sql
+++ portal/scripts/5.8.0/ALTER_OAW_5.8.0.sql
@@ -1,5 +1,6 @@
 INSERT INTO tguidelines values (11, 'observatorio-une-en2019_pdf.xml');
 INSERT INTO cartucho values (11, 'es.inteco.accesibilidad.CartuchoAccesibilidad', 1 , 'UNE-EN-2019-PDF', 15, 50, 11);
+INSERT INTO usuario_cartucho(1,11);
 
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
