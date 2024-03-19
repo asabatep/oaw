8505249d431e70cc94877fa6f8412054f53c1400
victor_cima <victor.cima@fundacionctic.org>
2024-03-19 16:15:04 +0100

    Alter script 5.8.0

--- portal/scripts/5.8.0/ALTER_OAW_5.8.0.sql
+++ portal/scripts/5.8.0/ALTER_OAW_5.8.0.sql
@@ -1,6 +1,6 @@
 INSERT INTO tguidelines values (11, 'observatorio-une-en2019_pdf.xml');
 INSERT INTO cartucho values (11, 'es.inteco.accesibilidad.CartuchoAccesibilidad', 1 , 'UNE-EN-2019-PDF', 15, 50, 11);
-INSERT INTO usuario_cartucho(1,11);
+INSERT INTO usuario_cartucho values(1,11);
 
 CREATE TABLE `observatorio_validator` (
   `status` tinyint(1) NOT NULL,
