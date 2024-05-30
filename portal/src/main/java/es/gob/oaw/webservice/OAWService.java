a129cd6eea0e54654dff54331e008b1d359848dd
Pablo <pablo.gonzalez@fundacionctic.org>
2024-05-30 09:06:03 +0200

    Update ValidationRequestJson

--- portal/src/main/java/es/gob/oaw/webservice/OAWService.java
+++ portal/src/main/java/es/gob/oaw/webservice/OAWService.java
@@ -74,14 +74,14 @@ public class OAWService {
 		EvaluatorUtility.initialize();
 		Evaluation evaluation = null;
 		// Validación de código fuente (Plugin de CMS, Plugin de Navegador)
-		if (!StringUtils.isBlank(validationRequestDTO.getHtmlContent())) {
-			byte[] decodedBytes = Base64.getDecoder().decode(validationRequestDTO.getHtmlContent().trim());
+		if (!es.inteco.common.utils.StringUtils.isUrl(validationRequestDTO.getContent())) {
+			byte[] decodedBytes = Base64.getDecoder().decode(validationRequestDTO.getContent().trim());
 			String sourceCode = new String(decodedBytes);
 			checkAccessibility.setContent(sourceCode);
 			evaluation = EvaluatorUtils.evaluateContent(checkAccessibility, defaultLanguage);
 			// Validación por url (Validador de URL). Esta opción hace uso del motor-js para renderizar la página web a analizar
-		} else if (!StringUtils.isBlank(validationRequestDTO.getUrl())) {
-			checkAccessibility.setUrl(validationRequestDTO.getUrl());
+		} else {
+			checkAccessibility.setUrl(validationRequestDTO.getContent());
 			evaluation = EvaluatorUtils.evaluate(checkAccessibility, defaultLanguage);
 		}
 		if (Objects.nonNull(evaluation)) {
