53518d932ed6936c88863510e387952030eaf3a1
Pablo Gonzalez <pablo.gonzalez@fundacionctic.org>
2025-01-28 11:42:45 +0100

    Added timeout exception explicit control

--- intavcore/src/main/java/es/inteco/accesibilidad/CartuchoAccesibilidad.java
+++ intavcore/src/main/java/es/inteco/accesibilidad/CartuchoAccesibilidad.java
@@ -51,6 +51,7 @@ import es.inteco.intav.utils.EvaluatorUtils;
 import es.inteco.plugin.Cartucho;
 import es.inteco.plugin.dao.DataBaseManager;
 import org.apache.commons.codec.binary.Base64;
+import java.net.SocketTimeoutException;
 
 /**
  * Implementación de un cartucho que analiza las urls, así como el contenido de las páginas y clasificarlas como maliciosas o no.
@@ -120,7 +121,6 @@ public class CartuchoAccesibilidad extends Cartucho {
 				}
 				else {
 					DataBaseManager.closeConnection(c);
-					Logger.putLog("CONTENT: " + new String(Base64.decodeBase64(checkAccesibility.getContent())), CartuchoAccesibilidad.class, Logger.LOG_LEVEL_WARNING);
 					if ((checkAccesibility.getUrl() != null && !checkAccesibility.getUrl().contains(".pdf")) && !(new String(Base64.decodeBase64(checkAccesibility.getContent())).contains("%PDF"))) {
 						EvaluatorUtils.evaluateContent(checkAccesibility, pmgr.getValue("crawler.core.properties", "check.accessibility.default.language"));
 					}
@@ -133,7 +133,7 @@ public class CartuchoAccesibilidad extends Cartucho {
 		} catch (Exception e) {
 			Log.error("EXCEPTION: " + e.getMessage());
 			Logger.putLog("Excepcion: ", CartuchoAccesibilidad.class, Logger.LOG_LEVEL_ERROR, e);
-			if (e instanceof IOException){
+			if (e instanceof IOException || e instanceof SocketTimeoutException){
 				throw new HTTPException(500);
 			}
 		}
