6b00a8d35c99f00e7399541493a6d7e90064b0d4
Albert <101092983+asabatep@users.noreply.github.com>
2025-05-29 09:43:42 +0200

    move certgen to entrypoint

--- motor-js/nginx/nginx-entrypoint.sh
+++ motor-js/nginx/nginx-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 if ! test -f /etc/nginx/ssl/server.crt ; then
+    apk add openssl
     mkdir -vp /etc/nginx/ssl
+    cd /etc/nginx/ssl
     openssl genrsa -out server.key 2048
     openssl req -new -key server.key -out server.csr -subj "/C=ES/ST=ZZ/L=ZZ/O=oaw/OU=oaw/CN=localhost"
     openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
