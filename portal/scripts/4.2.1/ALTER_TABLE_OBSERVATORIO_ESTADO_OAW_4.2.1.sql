aa148cd6bebe1b93a6acfc6dae14e23daab04e44
Albert <101092983+asabatep@users.noreply.github.com>
2025-05-29 08:41:00 +0200

    multiple sql script fixes
    - fix varchar being too big in
      ALTER_TABLE_OBSERVATORIO_ESTADO_OAW_4.2.1.sql
    - fix duplicate primary key in ALTER_OAW_5.1.0.sql
    - add missing eol
    - add missing semicolons

--- portal/scripts/4.2.1/ALTER_TABLE_OBSERVATORIO_ESTADO_OAW_4.2.1.sql
+++ portal/scripts/4.2.1/ALTER_TABLE_OBSERVATORIO_ESTADO_OAW_4.2.1.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `observatorio_estado` CHANGE `ultima_url` `ultima_url` VARCHAR(8000);
-ALTER TABLE `observatorio_estado` CHANGE `actual_url` `actual_url` VARCHAR(8000);
+ALTER TABLE `observatorio_estado` CHANGE `ultima_url` `ultima_url` VARCHAR(1024);
+ALTER TABLE `observatorio_estado` CHANGE `actual_url` `actual_url` VARCHAR(1024);
