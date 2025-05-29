9e0065d1e024c90d3d596156677c30c6f7487525
Albert <101092983+asabatep@users.noreply.github.com>
2025-05-29 17:48:24 +0200

    skip building the nginx image

--- motor-js/nginx/35-mk-certs.sh
+++ motor-js/nginx/35-mk-certs.sh
@@ -8,5 +8,3 @@ if ! test -f /etc/nginx/ssl/server.crt ; then
     openssl req -new -key server.key -out server.csr -subj "/C=ES/ST=ZZ/L=ZZ/O=oaw/OU=oaw/CN=localhost"
     openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
 fi
-
-exec nginx -g "daemon off;"
